9def4652629fffc1f51070b3be30855e3056fa9e
sandeepvarma8466 <svarma852@gmail.com>
2022-08-04 22:23:08 +0530

    Hello World Spring application

---
+++ src/test/java/com/blz/helloworld1/HelloWorld1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blz.helloworld1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorld1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
